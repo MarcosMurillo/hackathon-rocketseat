ba5e387ccb87016fa847ed685134dd72bf6327d6
Felipe Chierice <felipe.souza@ies2.com.br>
2019-11-27 08:05:35 -0300

    Resoluçao de conflitos e warns

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -8,7 +8,7 @@ export default function HomePage() {
     <>
       <div className="home">
         <nav className="c-navbar fixed-top">
-          <a className="navbar-brand" href="#"><img class="c-brand" src={logo} alt="Logo"/></a>
+          <a className="navbar-brand" href="/"><img class="c-brand" src={logo} alt="Logo"/></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -16,24 +16,24 @@ export default function HomePage() {
           <div className="collapse navbar-collapse" id="conteudoNavbarSuportado">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(página atual)</span></a>
+                <a className="nav-link" href="/">Home <span className="sr-only">(página atual)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="/">Link</a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Dropdown
                 </a>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">Ação</a>
-                  <a className="dropdown-item" href="#">Outra ação</a>
+                  <a className="dropdown-item" href="/">Ação</a>
+                  <a className="dropdown-item" href="/">Outra ação</a>
                   <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">Algo mais aqui</a>
+                  <a className="dropdown-item" href="/">Algo mais aqui</a>
                 </div>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="#">Desativado</a>
+                <a className="nav-link disabled" href="/">Desativado</a>
               </li>
             </ul>
           </div>
