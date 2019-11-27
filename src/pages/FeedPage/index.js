c9ba50a8a79a230c9ccdc3121d5277d75f306a03
rdourado92 <r.dourado007@gmail.com>
2019-11-27 10:53:22 -0300

    FeedPage

--- src/pages/FeedPage/index.js
+++ src/pages/FeedPage/index.js
@@ -1,36 +1,42 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { inject, observer } from "mobx-react";
-import { FaEdit, FaStar, FaPlus } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
-// import { Container } from './styles';
+import logo from "../../images/logo.png";
 
-export default function FeedPage() {
-  // observer(function ProfilePage({ rootStore }) {
-  //   useEffect(() => {
-  //     rootStore.userStore.loadOne(0);
-  //   }, [rootStore.userStore]);
+export default inject("rootStore")(
+  observer(function FeedPage({ rootStore }) {
+    useEffect(() => {
+      rootStore.userStore.loadOne(0);
+    }, [rootStore.userStore]);
 
-  //   function handleAddSkill() {
-  //     return alert(`Parabéns, você clicou no botão`);
-  //   }
-  //   const fullName = rootStore.userStore.one
-  //     ? rootStore.userStore.one.fullName
-  //     : "";
-  //   const avatar = rootStore.userStore.one
-  //     ? rootStore.userStore.one.avatar
-  //     : "";
+    const fullName = rootStore.userStore.one
+      ? rootStore.userStore.one.fullName
+      : "";
+    const avatar = rootStore.userStore.one
+      ? rootStore.userStore.one.avatar
+      : "";
 
-  return (
-    <>
-      <div className="profile-header">
-        {/* <img src={} alt="" /> */}
-        <span>
-          Editar perfil <FaEdit />
-        </span>
-        <span>
-          Patente <FaStar />
-        </span>
-      </div>
-    </>
-  );
-}
+    return (
+      <>
+        <div className="profile-header">
+          <a className="navbar-brand" href="/">
+            <img className="c-brand" src={logo} alt="Logo" />
+          </a>
+
+          <img src={avatar} alt={fullName} style={{ borderRadius: "50%" }} />
+          <Link to="/profile">{fullName}</Link>
+
+          <div className="search-box">
+            <input type="search" name="search" placeholder="Pesquisar" />
+            <button>
+              <FaSearch />
+            </button>
+          </div>
+        </div>
+        <div className="profile-info"></div>
+      </>
+    );
+  })
+);
