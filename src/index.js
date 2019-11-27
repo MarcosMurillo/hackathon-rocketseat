552b5100e3d7324ba29e59a702b877f7d9c2d691
Alex Marques <falecomle@gmail.com>
2019-11-27 06:10:36 -0500

    correção homePage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-<<<<<<< HEAD
 
 import './style.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import {} from "react-icons/fa";
->>>>>>> 8840e3fd03668fffcd68a0682649417ed83f0ec8
 
 ReactDOM.render(<App />, document.getElementById("root"));
