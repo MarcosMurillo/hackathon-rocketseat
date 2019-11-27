82ba2d859e74a51cf3c1f8ac4e79a03dd419fcf6
Alex Marques <falecomle@gmail.com>
2019-11-27 06:53:27 -0500

    Home update

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter, FaBars } from "react-icons/fa";
 import logo from '../../images/logo.png';
 
 
@@ -7,33 +7,22 @@ export default function HomePage() {
   return (
     <>
       <div className="home">
-        <nav className="c-navbar fixed-top">
+        <nav className="c-navbar navbar-expand-lg fixed-top">
           <a className="navbar-brand" href="/"><img class="c-brand" src={logo} alt="Logo"/></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-            <span className="navbar-toggler-icon"></span>
+          <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+            <FaBars className="i-bars" />
           </button>
 
-          <div className="collapse navbar-collapse" id="conteudoNavbarSuportado">
+          <div className="collapse navbar-collapse ml-5" id="conteudoNavbarSuportado">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span className="sr-only">(página atual)</span></a>
+                <a className="c-nav-link" href="/">Home<span className="sr-only">(página atual)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="/">Ação</a>
-                  <a className="dropdown-item" href="/">Outra ação</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="/">Algo mais aqui</a>
-                </div>
+                <a className="c-nav-link" href="/">Quem somos</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="/">Desativado</a>
+                <a className="c-nav-link" href="/">Contato</a>
               </li>
             </ul>
           </div>
@@ -41,7 +30,10 @@ export default function HomePage() {
       
         <div className="container">
           <div className="row">
-            
+            <div className="c-intro d-flex flex-column justify-content-center">
+              <h1 className="c-intro__tit">Aprenda. Ensine. Trabalhe.</h1>
+              <h2 className="c-intro__sub-tit">O plataforma definitiva para iniciantes e sêniors da programação.</h2>
+            </div>
           </div>
         </div>
 
@@ -50,7 +42,8 @@ export default function HomePage() {
             <div className="row">
               <div className="c-list-icons d-flex">
                 <FaFacebook className="i-facebook mr-3" />
-                <FaInstagram className="i-instagram" />
+                <FaInstagram className="i-instagram mr-3" />
+                <FaTwitter className="i-twitter" />
               </div>
             </div>
           </div>
