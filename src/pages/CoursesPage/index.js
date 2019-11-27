ae2860259e265f8d460293bc4112e4742e422e11
Felipe Chierice <felipe.souza@ies2.com.br>
2019-11-27 11:27:57 -0300

    pagina de cursos

--- src/pages/CoursesPage/index.js
+++ src/pages/CoursesPage/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { inject, observer } from "mobx-react";
-import { FaEdit, FaStar, FaPlus } from "react-icons/fa";
+// import { toJS } from 'mobx'
+
+import TechnologyComponent from '../../components/TechnologyComponent'
 
 export default inject("rootStore")(
   observer(function ProfilePage({ rootStore }) {
@@ -8,15 +10,22 @@ export default inject("rootStore")(
       rootStore.courseStore.loadAll()
     }, [rootStore.courseStore]);
 
+    // console.log(rootStore.courseStore.all && toJS(rootStore.courseStore.all[0].relatedTechnologies))
 
     return (
       <>
         { rootStore.courseStore.all ? 
           rootStore.courseStore.all.map(course => (
-            <div className="card m-3">
-              <div class="card-body">
-                <h5 class="card-title">{course.title}</h5>
-                <p class="card-text">{course.description}</p>
+            <div className="card m-3" key={course.id}>
+              <div className="card-body">
+                <h5 className="card-title mr-3">{course.title}</h5>
+                
+                <p className="card-text">{course.description}</p>
+                <a className="btn btn-success" href={course.url}>Acessar</a>
+                <hr />
+                <div className="d-flex flex-row">
+                  {course.relatedTechnologies.map(technology => <TechnologyComponent key={technology.id} technology={technology} />)}
+                </div>
               </div>
             </div>
           ))
